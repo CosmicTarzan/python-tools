56cda7958839cba091eba7e319e922bda95bd663
CosmicTarzan <adameko@gmail.com>
2024-01-04 01:16:17 +0100

    Update add_zeros_to_filenames.py

--- add_zeros_to_filenames.py
+++ add_zeros_to_filenames.py
@@ -1,10 +1,13 @@
 import os
- 
-os.chdir(string)
+
+loc = 'C:\\Users\\Adam\\OneDrive\\Dokumenty\\GitHub\\Leetcode-java'
+ext = '.java'
+
+os.chdir(loc)
  
 for count, f in enumerate(os.listdir()):
     f_name, f_ext = os.path.splitext(f)
-    f_name = "0" + str(f_name)
- 
-    new_name = f'{f_name}{f_ext}'
-    os.rename(f, new_name)
+    if f_ext == ext:
+        f_name = "0" + str(f_name)
+        new_name = f'{f_name}{f_ext}'
+        os.rename(f, new_name)
